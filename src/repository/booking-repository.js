3144ee963b883439868526a8317884e6e9dfa9f4
Ayush <ayushchopra0@gmail.com>
2023-01-02 22:37:00 +0530

    implemented booking and inter service communication through http

--- src/repository/booking-repository.js
+++ src/repository/booking-repository.js
@@ -7,6 +7,7 @@ const { ValidationError, AppError } = require('../utils/error/index');
 class BookingRepository{
         async create(data){
             try {
+                console.log(data.flightId);
                 const booking = await Booking.create(data);
                 return booking;
             } catch (error) {
@@ -20,7 +21,26 @@ class BookingRepository{
                     StatusCodes.INTERNAL_SERVER_ERROR
                 )
             }
-        }        
+        }
+        
+        async update(bookingId  , data){
+            try {
+                await Booking.update(data , {
+                    where : {
+                        id : bookingId
+                    }
+                });
+                return true;          
+            } catch (error) {
+                throw AppError(
+                    'RepositoryError',
+                    'Cannot update Booking',
+                    'There was some issue updating the booking, please try again later',
+                    StatusCodes.INTERNAL_SERVER_ERROR
+                )
+            }
+
+        }
 }
 
 module.exports = BookingRepository;
--- src/service/booking-service.js
+++ src/service/booking-service.js
@@ -6,20 +6,44 @@ const { ServiceError } = require('../utils/error/index');
 
 class BookingService {
         constructor(){
-            this.bookingRepository = BookingRepository;
+            this.bookingRepository = new BookingRepository();
         }
 
         async createBooking(data){
             try {
                 const flightId = data.flightId; 
+                // console.log("flight id " , flightId);
+                // console.log('before flight fetch');
                 let getFlightRequestURL = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${flightId}`;
                 const flight = await axios.get(getFlightRequestURL);
-                // console.log("data is :" , flight.data);
-                return flight.data.data;
+                // console.log(flight);
+                const flightData = flight.data.data;
+                // console.log(flightData);
+                let priceOfFlight = flightData.price;
+                // console.log('after flight fetch' , priceOfFlight);
+                // console.log("price is " , priceOfFlight);
+                if(data.noOfSeats > flightData.totalSeats){
+                   throw new ServiceError('Something went wrong in booking process ' , 'Insufficient seats in the flight');
+                }
+                const totalCost = priceOfFlight*data.noOfSeats;
+                const bookingPayload = {...data , totalCost   , status : 'Booked'};
+                // bookingPayload = {...bookingPayload , status : 'Booked'};
+                console.log("bfi",bookingPayload.flightId)
+                const booking = await this.bookingRepository.create(bookingPayload);
+                console.log('booking' , booking);
+                const updateFlightRequestURL = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${flightId}`;
+                await axios.patch(updateFlightRequestURL , {
+                    totalSeats : flightData.totalSeats - booking.noOfSeats
+                });
+
+                return booking;
 
             } catch (error) {
                 console.log(error);
-                throw new ServiceError()
+                if(error.name == 'SequelizeValidationError' || error.name =='RepositoryError' || error.name =='ServiceError'){
+                    throw error;
+                }
+                throw new ServiceError();
             }
         }
 }
